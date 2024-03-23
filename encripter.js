4b6277e1dcc70d87595823bff350e0ff757cfc64
dangalcan <megamagolas@gmail.com>
2024-03-23 12:47:06 +0100

    fixed issues when encripting

--- encripter.js
+++ encripter.js
@@ -3,37 +3,35 @@ const crypto = require('crypto');
 const { Transform } = require('stream');
 const csv = require('fast-csv');
 
-let columnsToEncrypt = []; 
-
-function encryptValue(value) {
-    const algorithm = 'aes-256-cbc';
-    const key = crypto.randomBytes(32);
-    const iv = crypto.randomBytes(16);
+async function encriptCsv(csvName, columns){
+    let columnsToEncrypt = columns; 
+    class EncryptTransform extends Transform {
+        constructor() {
+            super({ objectMode: true });
+        }
     
-    const cipher = crypto.createCipheriv(algorithm, key, iv);
-    let encrypted = cipher.update(value, 'utf8', 'hex');
-    encrypted += cipher.final('hex');
-    return iv.toString('hex') + encrypted;
-}
-
-class EncryptTransform extends Transform {
-    constructor() {
-        super({ objectMode: true });
+        _transform(row, encoding, callback) {
+            columnsToEncrypt.forEach((column) => {
+                if (row[column]) {
+                    row[column] = encryptValue(row[column]);
+                }
+            });
+            this.push(row);
+            callback();
+        }
     }
 
-    _transform(row, encoding, callback) {
-        columnsToEncrypt.forEach((column) => {
-            if (row[column]) {
-                row[column] = encryptValue(row[column]);
-            }
-        });
-        this.push(row);
-        callback();
+    function encryptValue(value) {
+        const algorithm = 'aes-256-cbc';
+        const key = crypto.randomBytes(32);
+        const iv = crypto.randomBytes(16);
+        
+        const cipher = crypto.createCipheriv(algorithm, key, iv);
+        let encrypted = cipher.update(value, 'utf8', 'hex');
+        encrypted += cipher.final('hex');
+        return iv.toString('hex') + encrypted;
     }
-}
 
-function encriptCsv(csvName, columns){
-    columnsToEncrypt = columns; 
     fs.createReadStream(csvName)
     .pipe(csv.parse({ headers: true }))
     .pipe(new EncryptTransform())
@@ -44,5 +42,16 @@ function encriptCsv(csvName, columns){
     });
 
 }
+
+// columnsToEncrypt = ['firstname', 'lastname']; 
+//     fs.createReadStream('users.csv')
+//     .pipe(csv.parse({ headers: true }))
+//     .pipe(new EncryptTransform())
+//     .pipe(csv.format({ headers: true }))
+//     .pipe(fs.createWriteStream('encrypted_users.csv'))
+//     .on('finish', () => {
+//         console.log('CSV excripted successfully');
+//     });
+
 
 module.exports = { encriptCsv };
